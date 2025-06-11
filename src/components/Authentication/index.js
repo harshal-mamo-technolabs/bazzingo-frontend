9cfb73f531ac43785878f68175eba03643952d17
harshal <github.harshal@gmail.com>
2025-06-11 19:24:35 +0530

    completed forget password functionlity

--- src/components/Authentication/index.js
+++ src/components/Authentication/index.js
@@ -1,5 +1,6 @@
 import LoginForm from './LoginForm';
 import ForgetPasswordEmailInputForm from './ForgetPasswordEmailInputForm';
 import SignupForm from './SignupForm';
+import ResetPasswordForm from './ResetPasswordForm';
 
-export { LoginForm, ForgetPasswordEmailInputForm, SignupForm };
+export { LoginForm, ForgetPasswordEmailInputForm, SignupForm, ResetPasswordForm };
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import { Signup, Login, ForgotPassword } from "./pages";
+import { Signup, Login, ForgotPassword, ResetPassword } from "./pages";
 
 const router = createBrowserRouter([
     {
@@ -18,6 +18,10 @@ const router = createBrowserRouter([
     {
         path: "/forgot-password",
         element: <ForgotPassword />,
+    },
+    {
+        path: "/forgot-password/:token",
+        element: <ResetPassword />,
     }
 ]);
 
