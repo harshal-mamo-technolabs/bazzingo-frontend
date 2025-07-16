8fd483376f70cebef4882537ad65c51510bc0118
jay100202 <bhailumandaliya11@gmail.com>
2025-07-16 12:51:28 +0530

    refactor: remove dashboard-bridge and update import path for Dashboard component

--- src/pages/dashboard-bridge.js
+++
@@ -1,2 +0,0 @@
-// This file serves as a bridge to solve case sensitivity issues
-export { default } from './Dashboard.jsx';
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import TermsOfUse from './TermsOfUse';
 import UserDashboard from './UserDashboard';
 import Assessments from './Assessments';
 import Games from './Games';
-import Dashboard from './dashboard-bridge';
+import Dashboard from './Dashboard';
 import Profile from './Profile';
 import Statistics from './Statistics';
 import Leadboard from './Leadboard';
